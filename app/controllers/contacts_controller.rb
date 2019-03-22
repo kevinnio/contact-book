b78bf725ea0f966dda552da0ef86f4a600f44053
Kevin Perez <kevindperezm@gmail.com>
2019-03-22 15:44:37 -0600

    allow user to edit alredy existing contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   before_action :load_contacts, only: :index
+  before_action :load_contact, only: [:edit, :update]
 
   def index; end
 
@@ -11,10 +12,20 @@ class ContactsController < ApplicationController
     @contact = Contact.create!(contact_params)
   end
 
+  def edit; end
+
+  def update
+    @contact.update(contact_params)
+  end
+
   protected
 
   def load_contacts
     @contacts = Contact.order(:firstname, :lastname)
+  end
+
+  def load_contact
+    @contact = Contact.find(params[:id])
   end
 
   def contact_params
