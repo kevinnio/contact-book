db88a5224bd057a812ec9bb4bc8279b3f1a27a2f
Kevin Perez <kevindperezm@gmail.com>
2019-03-22 17:20:02 -0600

    customize devise views so they match app style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
   has_many :contacts
 end
